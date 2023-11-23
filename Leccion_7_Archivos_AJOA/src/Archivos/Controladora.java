4bee96906bf2104a30382508f88f3668c850d288
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 23:12:58 -0600

    avances finales de la primera parte de la practica

--- Leccion_7_Archivos_AJOA/src/Archivos/Controladora.java
+++ Leccion_7_Archivos_AJOA/src/Archivos/Controladora.java
@@ -5,26 +5,26 @@ import java.io.*;
 public class Controladora {
 	
 	public static void main(String[] args) {
-		
+
 		WebMaker generador = new WebMaker();
-		
+
 		try {
-		PrintWriter escritor = new PrintWriter("index.txt");
-		Scanner lector = new Scanner(new FileReader("form.txt"));
+			PrintWriter escritor = new PrintWriter("index.html");
+			Scanner	lector = new Scanner (new FileReader("form.txt"));
+			escritor.println(generador.imprimirEncabezado());
 		
-		escritor.println(generador.imprimirEncabezado());
-		escritor.println(generador.imprimirTexto("Un texto de prueba :D"));
-		escritor.println(generador.imprimirTexto("Pagina web para expresar mi odio hacia las tareas"));
+			escritor.println(generador.imprimirTexto("Mi pagina web (soy el gato)"));
+			escritor.println(generador.insertarImagen("Rainworld.png"));
+
+			while(lector.hasNext())	{
+				escritor.println(lector.nextLine());
+			}
+			escritor.println(generador.imprimirCierre());
+			lector.close();
+			escritor.close();
 		
-		
-		while(lector.hasNext())	{
-			escritor.println(lector.nextLine());
-		}
-		escritor.println(generador.imprimirCierre());
-		lector.close();
-		escritor.close();
-		}catch(FileNotFoundException e){
+		}catch(FileNotFoundException e) {
+			
 		}
 	}
-
 }
