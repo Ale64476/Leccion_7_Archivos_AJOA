584fb32878dee4c7fbf3bbf712c036fcb632ed0c
Ale64476 <148830719+Ale64476@users.noreply.github.com>
2023-11-22 21:48:24 -0600

    agregue el bloque try catch par apoder compilar y cambie el txt por html

--- Leccion_7_Archivos_AJOA/src/Archivos/Controladora.java
+++ Leccion_7_Archivos_AJOA/src/Archivos/Controladora.java
@@ -5,12 +5,26 @@ import java.io.*;
 public class Controladora {
 	
 	public static void main(String[] args) {
+		
 		WebMaker generador = new WebMaker();
+		
+		try {
 		PrintWriter escritor = new PrintWriter("index.txt");
 		Scanner lector = new Scanner(new FileReader("form.txt"));
 		
 		escritor.println(generador.imprimirEncabezado());
-
+		escritor.println(generador.imprimirTexto("Un texto de prueba :D"));
+		escritor.println(generador.imprimirTexto("Pagina web para expresar mi odio hacia las tareas"));
+		
+		
+		while(lector.hasNext())	{
+			escritor.println(lector.nextLine());
+		}
+		escritor.println(generador.imprimirCierre());
+		lector.close();
+		escritor.close();
+		}catch(FileNotFoundException e){
+		}
 	}
 
 }
